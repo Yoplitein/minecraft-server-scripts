578112d0454b97c0939253ac0213c7623206472a
Steven Dwy <yoplitein+git@gmail.com>
2022-06-03 02:32:59 -0700

    Print whether using new or legacy launch method

--- systemd/start.sh
+++ systemd/start.sh
@@ -3,9 +3,10 @@
 # Forge 1.16+
 forgeJVMFlags=( libraries/net/minecraftforge/forge/*/unix_args.txt )
 if [ -e $forgeJVMFlags ]; then
+    echo "Launching using Forge 1.16+ method ($forgeJVMFlags)"
     exec env java -Xms${MEM_MIN} -Xmx${MEM_MAX} $JVM_ARGS @$forgeJVMFlags $MC_ARGS
 elif [ -e server.jar ]; then
-    # legacy
+    echo "Launching using legacy server.jar method"
     exec env java -Xms${MEM_MIN} -Xmx${MEM_MAX} $JVM_ARGS -jar server.jar $MC_ARGS
 else
     echo "Don't know how to launch server in $PWD"
