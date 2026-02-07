008ec1e7903cfde93c120c6b661c695a2bdfcd9a
Steven Dwy <yoplitein+git@gmail.com>
2026-02-06 21:33:55 -0800

    Support NeoForge launch arguments

--- systemd/start.sh
+++ systemd/start.sh
@@ -2,9 +2,13 @@
 
 # Forge 1.16+
 forgeJVMFlags=( libraries/net/minecraftforge/forge/*/unix_args.txt )
+neoforgeJVMFlags=( libraries/net/neoforged/neoforge/*/unix_args.txt )
 if [ -e $forgeJVMFlags ]; then
     echo "Launching using Forge 1.16+ method ($forgeJVMFlags)"
     exec env java -Xms${MEM_MIN} -Xmx${MEM_MAX} $JVM_ARGS @$forgeJVMFlags $MC_ARGS
+elif [ -e $neoforgeJVMFlags ]; then
+    echo "Launching using NeoForge method ($neoforgeJVMFlags)"
+    exec env java -Xms${MEM_MIN} -Xmx${MEM_MAX} $JVM_ARGS @$neoforgeJVMFlags $MC_ARGS
 elif [ -e server.jar ]; then
     echo "Launching using legacy server.jar method"
     exec env java -Xms${MEM_MIN} -Xmx${MEM_MAX} $JVM_ARGS -jar server.jar $MC_ARGS
